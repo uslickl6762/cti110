ffcb544da70be62d4397b0271257eb4a5b5ef380
uslickl6762 <160055975+uslickl6762@users.noreply.github.com>
2024-02-15 14:37:21 -0500

    Add files via upload

---
+++ P1LAB1_UslickLauren.py
@@ -0,0 +1,10 @@
+# Lauren Uslick
+# 2/15/2024
+# P1LAB1
+# This program will display my name in a welcome message
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+
+print("Hello,",fname,lname+"! Welcome to CTI-110")
